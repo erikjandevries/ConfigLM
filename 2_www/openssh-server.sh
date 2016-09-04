b54e48b83f11e44a8e6f6fb9b8ca9593504f4fd9
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-09-04 21:39:36 +0200

    OpenSSH authentication with key only (no passwords)

--- 2_www/openssh-server.sh
+++ 2_www/openssh-server.sh
@@ -1,3 +1,6 @@
 echo_section "Installing OpenSSH Server"
 
 sudo apt-get install -y openssh-server
+
+sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+sudo service ssh restart
