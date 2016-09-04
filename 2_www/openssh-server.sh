02a75b95338eb33f8f9f9f44898221d192c8f0a1
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-09-04 21:55:59 +0200

    Updated OpenSSH configuration

--- 2_www/openssh-server.sh
+++ 2_www/openssh-server.sh
@@ -1,6 +1,12 @@
 echo_section "Installing OpenSSH Server"
 
+echo_subsection "Installing latest security stack"
+sudo apt-get install xserver-xorg-lts-xenial libwayland-egl1-mesa-lts-xenial libgl1-mesa-glx-lts-xenial:i386 libgl1-mesa-glx-lts-xenial
+
+echo_subsection "Installing OpenSSH Server"
 sudo apt-get install -y openssh-server
 
+echo_section "Configuring OpenSSH Server for authentication with keys only"
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.defaults
 sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
 sudo service ssh restart
