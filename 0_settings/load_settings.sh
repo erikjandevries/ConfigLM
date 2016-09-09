a33f021f9216091efc8231d5a0854025e2a5b9ba
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-09-09 11:12:18 +0200

    Only prompt when needed

--- 0_settings/load_settings.sh
+++ 0_settings/load_settings.sh
@@ -1,20 +1,23 @@
 echo_section "Loading settings"
 
-echo_subsection "MariaDB"
-MARIADB_VERSION=5.5
+if [ $(dpkg-query -W -f='${Status}' mariadb-server 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo_subsection "MariaDB"
+  MARIADB_VERSION=5.5
 
-if [ "x$mariadb_root_password" == "x" ]; then
-  read -s -p "MariaDB database root password:" mariadb_root_password
-  echo
-  read -s -p "Please enter again:" mariadb_root_password2
-  echo
+  if [ "x$mariadb_root_password" == "x" ]; then
+    read -s -p "MariaDB database root password:" mariadb_root_password
+    echo
+    read -s -p "Please enter again:" mariadb_root_password2
+    echo
+  fi
+
+  while [ "$mariadb_root_password" != "$mariadb_root_password2" ]; do
+    echo "Passwords don't match!"
+    echo ""
+    read -s -p "MariaDB database root password:" mariadb_root_password
+    echo
+    read -s -p "Please enter again:" mariadb_root_password2
+    echo
+  done
 fi
-
-while [ "$mariadb_root_password" != "$mariadb_root_password2" ]; do
-  echo "Passwords don't match!"
-  echo ""
-  read -s -p "MariaDB database root password:" mariadb_root_password
-  echo
-  read -s -p "Please enter again:" mariadb_root_password2
-  echo
-done
