919bd33395933cc484495ccc0dd648a798c770e3
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-09-13 14:13:30 +0200

    Update

--- 2_www/apache.sh
+++ 2_www/apache.sh
@@ -1,6 +1,4 @@
 # https://www.digitalocean.com/community/tutorials/how-to-configure-nginx-as-a-reverse-proxy-for-apache
 echo_section "Installing Apache www-server"
 ensure_pkg apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1
-ensure_pkg libapache2-mod-wsgi
-
 sudo service apache2 restart
--- 9_private_config/apache_template.sh
+++ 9_private_config/apache_template.sh
@@ -15,7 +15,6 @@ sudo grep -q -F "$MYFQDN" /etc/hosts || sudo sed -i "s/127.0.0.1\tlocalhost/127.
 ensure_conf "ServerName $MYFQDN" /etc/apache2/apache2.conf -sudo
 replace_conf "Listen 80\n" "Listen $WWW_PORT\n" /etc/apache2/ports.conf -sudo
 
-sudo cp apache-default.conf /etc/apache2/sites-available/000-default.conf
 replace_conf "<VirtualHost \*:80>" "<VirtualHost \*:$WWW_PORT>" /etc/apache2/sites-available/000-default.conf -sudo
 sudo rm /etc/apache2/sites-enabled/000-default.conf
 ensure_sl /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/000-default.conf
