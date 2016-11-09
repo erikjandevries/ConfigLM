8a1ebbb092acfb39cd431619fd9c9c5dbec10e67
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-11-09 18:23:49 +0100

    Removed WLAN driver from blacklist

--- 1_linux/driver_WLAN.sh
+++ 1_linux/driver_WLAN.sh
@@ -1,3 +1,5 @@
 echo_section "Installing driver for WLAN USB"
+
+replace_conf 'blacklist bcm43xx' '#blacklist bcm43xx' /etc/modprobe.d/blacklist.conf -sudo
 ensure_pkg linux-firmware-nonfree
 sudo modprobe b43
